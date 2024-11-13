5bc6336593a01f0f0c7518dd2b85b5287fde5f05
Fernando Iglesias <nando.freitas.iglesias@gmail.com>
2024-11-13 15:46:41 -0400

    desafio realizado

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,8 @@
 import { ButtonContainer } from "./styles";
 import { IButtonProps } from "./types";
 
-const Button = ({ title, onClick }: IButtonProps) => {
-  return <ButtonContainer onClick={onClick}>{title}</ButtonContainer>;
+const Button = ({ children, onClick, disabled }: IButtonProps) => {
+  return <ButtonContainer onClick={onClick} disabled={disabled}>{children}</ButtonContainer>;
 };
 
 export default Button;
